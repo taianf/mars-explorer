01225beaaa677fc153513f292dadff7202c98858
Taian Fonseca Feitosa <taianmeca@gmail.com>
2020-10-15 20:30:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+Simple Spring Rest API
---
+++ src/test/java/br/com/taian/marsexplorer/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taian.marsexplorer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
